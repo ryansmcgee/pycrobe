e69ff4d5d0c73e5e071c51b7b22189eaa2702290
Ryan McGee <ryan@D-10-19-70-35.dhcp4.washington.edu>
2019-02-21 23:10:08 -0800

    Uploading version 0.1.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pycrobe',
-      version='0.1.3',
+      version='0.1.4',
       description='A simple package for simulations of microbial growth experiments with realistic sources of error.',
       url='https://github.com/ryansmcgee/pycrobe',
       author='Ryan Seamus McGee',
